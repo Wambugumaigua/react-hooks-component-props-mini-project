a5eb9d96ebad0c1ae0a947768382951d038be7ab
Alex Wambugu <wambugumaigua@DESKTOP-F32N3MJ>
2024-04-26 03:11:34 -0700

    complete lab

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header=({name})=>{
+  return (
+    <div className="App">
+      <header>
+        <h1>{name}</h1>
+     </header>
+</div>
+)
+}
+
+export default Header
